570b135ad25034f3acb70f0256bc23d899ca041e
herereadthis <herereadthis.github@gmail.com>
2022-07-14 23:59:54 -0400

    restructure

--- gloomer/foo.py
+++ gloomer/foo.py
@@ -1,5 +1,20 @@
+import os
+import json
+import requests
+
 from yum import utils
 
+from definitions import ROOT_DIR
+
+
+def fetch(url: str) -> list:
+    res = requests.get(url)
+    return json.loads(res.content)
 
 if __name__ == '__main__':
     print('hello world')
+
+    json_dump = os.path.join(ROOT_DIR, 'tmp', '1657290545.json')
+    print(json_dump)
+
+    aircraft_json = fetch(url=json_dump)
